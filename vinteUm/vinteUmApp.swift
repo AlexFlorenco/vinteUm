f0f27650e274100db1094603ab44dda744319ed0
Alexandre Floren;o <aflorenco13@gmail.com>
2023-04-08 19:31:13 -0300

    Initial Commit

---
+++ vinteUm/vinteUmApp.swift
@@ -0,0 +1,17 @@
+//
+//  vinteUmApp.swift
+//  vinteUm
+//
+//  Created by Alexandre Florenço on 08/04/23.
+//
+
+import SwiftUI
+
+@main
+struct vinteUmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
